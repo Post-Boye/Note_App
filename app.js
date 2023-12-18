9f18bafb834e4e3f7eca572f61526b2ab16a4c5d
Post-Boye <boyejustice64@gmail.com>
2023-12-18 22:50:44 +0000

    good work

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(session({
   }),
   //cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
   // Date.now() - 30 * 24 * 60 * 60 * 1000
-}));
+})); 
 
 app.use(passport.initialize());
 app.use(passport.session());
